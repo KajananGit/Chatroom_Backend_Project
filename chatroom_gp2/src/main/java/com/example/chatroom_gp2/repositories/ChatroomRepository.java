4b095c36b176fe43bd917e91730986549f788643
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-03-04 11:25:01 +0000

    added ChatroomRepository

---
+++ chatroom_gp2/src/main/java/com/example/chatroom_gp2/repositories/ChatroomRepository.java
@@ -0,0 +1,6 @@
+package com.example.chatroom_gp2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
+}
