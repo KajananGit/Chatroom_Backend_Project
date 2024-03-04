6592ebfeb957fd828f0a1f3a67b721c26676024a
Zakaria <zakjama0@gmail.com>
2024-03-04 11:40:49 +0000

    User model completed

--- chatroom_gp2/src/main/java/com/example/chatroom_gp2/ChatroomGp2Application.java
+++ chatroom_gp2/src/main/java/com/example/chatroom_gp2/ChatroomGp2Application.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatroomGp2Application {
-
-	public static void main(String[] args) {
+public class ChatroomGp2Application { public static void main(String[] args) {
 		SpringApplication.run(ChatroomGp2Application.class, args);
 	}
 
